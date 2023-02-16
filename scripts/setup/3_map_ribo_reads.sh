db37b58d0753ff8eb1a6b70424620e2b5e39ad01
jdcla <jimc@galaxy.UGent.be>
2023-02-16 13:14:23 +0100

    fix relative paths

--- scripts/setup/3_map_ribo_reads.sh
+++ scripts/setup/3_map_ribo_reads.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cd ../../data/ribo
 
-## prefetch --option-file sralist.txt
 while read -r line
 do
 	# init
-	dataset=$(echo $line | cut -f1)
-	adapter=$(echo $line | cut -f2)	
+	dataset=$(echo $line | cut -f 1 -d ' ')
+	adapter=$(echo $line | cut -f 2 -d ' ')	
 	echo $dataset
 	mkdir $dataset
 	cd $dataset
@@ -18,12 +18,12 @@ do
 	cutadapt -j 20 -m 20 -a $adapter ${dataset}.fastq -o "out/temp/${dataset}_trimmed.fq" > "out/temp/${dataset}_trimmed_report.txt"
 
 	# remove rRNA/tRNA/smRNA/smoRNA
-	STAR --genomeLoad NoSharedMemory --seedSearchStartLmaxOverLread .5   --genomeDir /data/jimc/genomes/GRCh38/v107/STAR/excl_RNA --readFilesIn $trimmed --outFilterMultimapNmax 1000 --outFilterMismatchNmax 2 --outFileNamePrefix out/temp/ --runThreadN 20 --outReadsUnmapped Fastx
+	STAR --genomeLoad NoSharedMemory --seedSearchStartLmaxOverLread .5   --genomeDir '../../genome/STAR/excl_RNA' --readFilesIn $trimmed --outFilterMultimapNmax 1000 --outFilterMismatchNmax 2 --outFileNamePrefix out/temp/ --runThreadN 20 --outReadsUnmapped Fastx
 	mv out/temp/Unmapped.out.mate1 $cleaned 
 	fastqc -t 10 $cleaned
 	
 	# align to genome, output mapping to transcriptome as well
-	STAR   --runThreadN 20   --genomeDir /data/jimc/genomes/GRCh38/v107/STAR   --genomeLoad NoSharedMemory   --readFilesIn $cleaned --outFileNamePrefix out/ --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM --outSAMattributes MD NH --outFilterMultimapNmax 10  --outMultimapperOrder Random --outFilterMismatchNmax 2   --seedSearchStartLmaxOverLread 0.5   --alignEndsType EndToEnd --outWigType bedGraph
+	STAR   --runThreadN 20   --genomeDir '../../genome/STAR'   --genomeLoad NoSharedMemory   --readFilesIn $cleaned --outFileNamePrefix out/ --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM --outSAMattributes MD NH --outFilterMultimapNmax 10  --outMultimapperOrder Random --outFilterMismatchNmax 2   --seedSearchStartLmaxOverLread 0.5   --alignEndsType EndToEnd --outWigType bedGraph
 
 	# cleanup
 	mv out/Aligned.sortedByCoord.out.bam out/${dataset}_aligned.bam
@@ -33,4 +33,3 @@ do
 	samtools view -h -o out/${dataset}_aligned_tran.sam out/${dataset}_aligned_tran.bam
 	cd ..
 done < "metadata.txt"
-
