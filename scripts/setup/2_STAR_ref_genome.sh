1657b10807679937a5d2951be9960883f8474b61
jdcla <jimc@galaxy.UGent.be>
2023-02-16 12:21:55 +0100

    fixes on uninit dir

--- scripts/setup/2_STAR_ref_genome.sh
+++ scripts/setup/2_STAR_ref_genome.sh
@@ -2,10 +2,11 @@
 
 cd ../../data/genome
 version="107"
-mkdir STAR
 
 # setting up reference genome for STAR mapping
+mkdir STAR
 STAR --runThreadN 10 --runMode genomeGenerate --genomeDir STAR/ --genomeFastaFiles pa/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbGTFfile Homo_sapiens.GRCh38.${version}.gtf --sjdbOverhang 35
 
 # set up redundant RNA folder
+mkdir STAR/excl_RNA
 STAR --runThreadN 10 --runMode genomeGenerate --genomeDir STAR/excl_RNA --genomeFastaFiles excl_RNA.fasta --genomeSAindexNbases 6
